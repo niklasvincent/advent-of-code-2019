f1f59f2176b6053f4e4c6a4e039c48a89401da1f
Niklas Lindblad <niklas@lindblad.info>
2019-12-02 21:33:41 +0100

    Add more comments to day 1 problem and make puzzle input a common library

---
+++ src/main/scala/common/PuzzleInput.scala
@@ -0,0 +1,15 @@
+package common
+
+import java.io.File
+
+import scala.io.Source
+
+object PuzzleInput {
+
+  def readIntegersFromFile(filename: String): Seq[Int] = {
+    val f = new File(getClass.getClassLoader.getResource(filename).getPath)
+
+    Source.fromFile(f).getLines().map(l => l.toInt).toSeq
+  }
+
+}
--- src/main/scala/day01/Solver.scala
+++ src/main/scala/day01/Solver.scala
@@ -1,17 +1,32 @@
 package day01
 
-import java.io.File
-
-import scala.io.Source
+import common.PuzzleInput
 
 object Solver extends App {
 
-  val f = new File(getClass.getClassLoader.getResource("day01/input1.txt").getPath)
-
+  /*
+   * Fuel required to launch a given module is based on its mass.
+   * Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+   */
   def fuelRequired(mass: Int): Int = {
     Math.round(mass / 3) - 2
   }
 
+  /*
+   * For each module mass, calculate its fuel and add it to the total.
+   * Then, treat the fuel amount you just calculated as the input mass and repeat the process,
+   * continuing until a fuel requirement is zero or negative.
+   *
+   * For example:
+   *
+   * At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2).
+   * 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel.
+   * So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+   *
+   * The fuel required by a module of mass 100756 and its fuel is:
+   * 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+   *
+   */
   def fuelRequiredIncludingFuel(mass: Int): Int = {
     def series(fuel: Seq[Int]): Seq[Int] = {
       val additionalFuel = fuelRequired(fuel.last)
@@ -26,9 +41,11 @@ object Solver extends App {
     series(Seq(fuelRequired(mass))).sum
   }
 
-  val totalFuelRequired = Source.fromFile(f).getLines().map(l => l.toInt).map(fuelRequired).sum
+  val input: Seq[Int] = PuzzleInput.readIntegersFromFile("day01/input1.txt")
 
-  val totalFuelRequiredIncludingFuel= Source.fromFile(f).getLines().map(l => l.toInt).map(fuelRequiredIncludingFuel).sum
+  val totalFuelRequired = input.map(fuelRequired).sum
+
+  val totalFuelRequiredIncludingFuel= input.map(fuelRequiredIncludingFuel).sum
 
   println(s"Total fuel required is $totalFuelRequired")
 
